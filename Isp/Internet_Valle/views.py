63541b9e5bdd3891c394504e024e6f29f13027f4
murillo6 <murillo.6@hotmail.com>
2023-10-30 16:45:44 -0300

    View Equipo y Servicio

--- Isp/Internet_Valle/views.py
+++ Isp/Internet_Valle/views.py
@@ -52,4 +52,84 @@ class ClienteDetailView (DeleteView):
         context['titulo'] = "Cliente"
         return context
 
-################### CLIENTE ###################
+################### Servicio ###################
+
+class ServicioCreateView(CreateView):
+    model = Servicio
+    #form_class = FormularioServicio
+   # success_url = reverse_lazy('servicio:servicio_crear')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Alta de Servicio"
+        return context
+
+class ServicioUpdateView(UpdateView):
+    model = Cliente
+    #form_class = FormularioServicio
+   # success_url = reverse_lazy('servicio:servicio_crear')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Modificar Cliente"
+        return context
+    
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.SUCCESS, 'Servicio Cliente con éxito')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print(form.errors)
+        print(form.non_field_errors())
+        messages.add_message(self.request, messages.ERROR, form.errors)
+        return super().form_invalid(form)
+
+class ServicioDetailView (DeleteView):
+    model = Cliente
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Cliente"
+        return context
+
+
+################### Equipo ###################
+
+class EquipoCreateView(CreateView):
+    model = Equipo
+    #form_class = FormularioEquipo
+   # success_url = reverse_lazy('equipo:equipo_crear')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Alta de Equipo"
+        return context
+
+class EquipoUpdateView(UpdateView):
+    model = Equipo
+     #form_class = FormularioEquipo
+   # success_url = reverse_lazy('equipo:equipo_crear')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Modificar Equipo"
+        return context
+    
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.SUCCESS, 'Equipo modificada con éxito')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print(form.errors)
+        print(form.non_field_errors())
+        messages.add_message(self.request, messages.ERROR, form.errors)
+        return super().form_invalid(form)
+
+class EquipoDetailView (DeleteView):
+    model = Equipo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Equipo"
+        return context
+    
