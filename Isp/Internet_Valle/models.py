ec8d48d8a9cd0c9c62c0d5447bdd153d44240526
Alen Morillo Meneses <alenmisaelmorillomeneses@gmail.com>
2023-11-08 17:34:34 -0300

    Arreglado models y agregado la clase Localidad

--- Isp/Internet_Valle/models.py
+++ Isp/Internet_Valle/models.py
@@ -18,6 +18,9 @@ class Equipo (models.Model):
     fecha_fin = models.DateField(blank=True)
     descripcion = models.CharField(max_length=500)
 
+class Localidad (models.Model): 
+    nombre = models.CharField(max_length=50)
+
 class Contrato (models.Model):
     cliente = models.ForeignKey(Cliente, related_name="contrato",on_delete=models.CASCADE)
     servicio = models.ForeignKey(Servicio,on_delete=models.CASCADE)
@@ -25,6 +28,7 @@ class Contrato (models.Model):
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField(blank=True)
     fecha_desconexion = models.DateField(blank=True)
+    localidad = models.ForeignKey(Localidad, related_name="localidad", on_delete=models.CASCADE)
 
 class Pago (models.Model):
     contrato = models.ForeignKey(Contrato, related_name="pago",on_delete=models.CASCADE)
@@ -33,12 +37,18 @@ class Pago (models.Model):
     fecha_vencimiento = models.DateField(blank=True)
     fecha_pago = models.DateField(blank=True)
 
+class Adicional (models.Model):
+    monto =  models.DecimalField(help_text="monto del servicio", max_digits=10, decimal_places=2)
+    descripcion = models.CharField(max_length=500)
+
 class Detalle_Pago (models.Model):
     pago = models.ForeignKey(Pago, related_name="DetalleDePago",on_delete=models.CASCADE)
     renglon = models.CharField(max_length=500)
     costo =  models.DecimalField(help_text="costo", max_digits=10, decimal_places=2)
+    adicional = models.ForeignKey(Adicional, null=True, blank=True )
 
-class adicionales (models.Model):
-    monto =  models.DecimalField(help_text="monto del servicio", max_digits=10, decimal_places=2)
-    descripcion = models.CharField(max_length=500)
+
+
+
+
 
