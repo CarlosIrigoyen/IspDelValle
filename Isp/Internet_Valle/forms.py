5ee8b90464b37422be547feb1f82c195897ff887
murillo6 <murillo.6@hotmail.com>
2023-11-15 16:58:11 -0300

    vista de altas

--- Isp/Internet_Valle/forms.py
+++ Isp/Internet_Valle/forms.py
@@ -24,23 +24,54 @@ class ServicioForm (forms.ModelForm):
     class Meta:
        model = Servicio  
        fields = '__all__'   
+       widgets = {
+           'velocidad': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Velocidad'}),
+           'fecha_fin': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha fin'}),
+           'monto': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Monto'}),
+       }
 
 class EquipoForm (forms.ModelForm):
     class Meta:
        model = Equipo  
-       fields = '__all__'          
+       fields = '__all__'     
+       widgets = {
+           'nombre': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Nombre'}),
+           'fecha_fin': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha fin'}),
+           'descripcion': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Descripcion'}),
+       }     
 
 class ContratoForm (forms.ModelForm):
     class Meta:
        model = Contrato  
-       fields = '__all__'          
+       fields = '__all__'     
+       widgets = {
+           'cliente': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Cliente'}),
+           'servicio': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Servicio'}),
+           'direccion': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Direccion'}),
+           'fecha_inicio': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha Inicio'}),
+           'fecha_fin': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha Fin'}),
+           'fecha_desconexion': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha Desconexion'}),
+           'localidad': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Localidad'}),
+       }     
 
 class PagoForm (forms.ModelForm):
     class Meta:
        model = Pago  
        fields = '__all__'   
+       widgets = {
+           'contrato': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Contrato'}),
+           'monto_total': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Monto Total'}),
+           'fecha_emision': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha Emision'}),
+           'fecha_vencimiento': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha Vencimiento'}),
+           'fecha_pago': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Fecha Pago'}),
+       }
 
 class AdicionalForm (forms.ModelForm):
     class Meta:
        model = Adicional  
-       fields = '__all__'   
+       fields = '__all__' 
+       widgets = {
+           'monto': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Monto'}),
+           'descripcion': forms.TextInput(attrs={'class': 'form-control form-control-user', 'placeholder': 'Descripcion'}),
+        }
+      
--- Isp/static/sb2/js/demo/datatables-demo.js
+++ Isp/static/sb2/js/demo/datatables-demo.js
@@ -1,4 +1,8 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+$(document).ready(function () {
+  $('#dataTable').DataTable({
+    "language": {
+      "url": "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
+    }
+  });
 });
